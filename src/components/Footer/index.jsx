bd66266680ff519d2c2728962af6327abe249a9b
Stente3 <stentem0103@gmail.com>
2023-05-24 09:14:40 -0500

    Completed "Footer" Component and Mobile Device Styles
        + Completed the development of the "Footer" component in the project (React).
        + Implemented all necessary styles for mobile devices (Sass).

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,17 +1,34 @@
 import React from 'react';
-import facebook from '/facebook-f.svg';
-import instagram from '/instagram.svg';
-import twitter from '/twitter.svg';
+import facebook from '/facebook.png';
+import instagram from '/instagram.png';
+import twitter from '/twitter.png';
+import './index.scss';
 
 const images = [facebook, instagram, twitter];
 
 const Footer = () => {
   return (
     <React.Fragment>
-      {images.map((item, index) => {
-        return <img key={index} src={item} alt='item' />;
-      })}
-      <p>&copy; Copyright Ping. All rights reserved.</p>
+      <footer className='footer'>
+        <div className='footer__imageContainer'>
+          {images.map((item, index) => {
+            return (
+              <div className='container'>
+                <img
+                  key={index}
+                  src={item}
+                  alt='item'
+                  width='25px'
+                  height='25px'
+                />
+              </div>
+            );
+          })}
+        </div>
+        <p className='footer__paragraph'>
+          &copy; Copyright Ping. All rights reserved.
+        </p>
+      </footer>
     </React.Fragment>
   );
 };
