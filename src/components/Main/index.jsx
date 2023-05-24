c901d81a35c5350e5a783cf3ba6ce0aa78cccaf4
Stente3 <stentem0103@gmail.com>
2023-05-24 08:08:20 -0500

    Completed "Main" Component and Mobile Device Styles
    + Completed the development of the "Main" component in the project (React).
    + Implemented all necessary styles for mobile devices (Sass).

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import Form from './Form';
+import Form from '../Form';
 import illustration from '/illustration-dashboard.png';
+import './index.scss';
 
 const Main = () => {
   return (
     <React.Fragment>
-      <main>
-        <h1>
+      <main className='main'>
+        <h1 className='main__header'>
           We are launching <span>soon!</span>
         </h1>
-        <p>Subscribe and get notified</p>
-        <Form />
-        <img src={illustration} alt='illustration dashboard' />
+        <p className='main__paragraph'>Subscribe and get notified</p>
+        <div className='main__buttonContainer'>
+          <Form />
+        </div>
+        <figure className='main__figure'>
+          <img src={illustration} alt='illustration dashboard' />
+        </figure>
       </main>
     </React.Fragment>
   );
