2181c4b81a36f6f37775e620e6d98e571f407e3c
Stente3 <stentem0103@gmail.com>
2023-05-30 18:01:56 -0500

    Add 'footer' component in JSX and Sass files

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -7,23 +7,25 @@ import './index.scss';
 const images = [facebook, instagram, twitter];
 
 const Footer = () => {
+  function handleMouse(event) {
+    const imgElement = event.currentTarget.querySelector('img');
+    imgElement.classList.toggle('brightness');
+  }
+
   return (
     <React.Fragment>
       <footer className='footer'>
         <div className='footer__imageContainer'>
-          {images.map((item, index) => {
-            return (
-              <div className='container'>
-                <img
-                  key={index}
-                  src={item}
-                  alt='item'
-                  width='25px'
-                  height='25px'
-                />
-              </div>
-            );
-          })}
+          {images.map((item, index) => (
+            <div
+              key={index}
+              className='container'
+              onMouseOver={handleMouse}
+              onMouseOut={handleMouse}
+            >
+              <img src={item} alt='item' width='25px' height='25px' />
+            </div>
+          ))}
         </div>
         <p className='footer__paragraph'>
           &copy; Copyright Ping. All rights reserved.
